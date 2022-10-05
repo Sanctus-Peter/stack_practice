b40abd4e136f1dda7d2be5e4ced4f84c4301238b
mamt4real <mamt4real@gmail.com>
2022-10-05 15:46:38 -0700

    Alot of fixes

--- 10-helpers.c
+++ 10-helpers.c
@@ -24,6 +24,9 @@ int is_number(char *str)
 {
 	if (!str)
 		return (0);
+	if (*str == '-' || *str == '+')
+		str++;
+
 	while (*str)
 	{
 		if (!_isdigit(*str))
@@ -31,4 +34,42 @@ int is_number(char *str)
 		str++;
 	}
 	return (1);
+}
+
+/**
+ * _strcmp - compares src with dst
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ * Return: -ve if dst < src
+ * +ve if dst > src
+ * 0 if src == dst
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0, diff = 0;
+
+	while (diff == 0)
+	{
+		diff = s1[x] - s2[x];
+		if (s1[x] == '\0' || s2[x] == '\0')
+			break;
+		x++;
+	}
+	return (diff);
+}
+
+/**
+ * arrlen - length of array
+ * @args: array
+ * Return: the length
+ */
+
+int arrlen(char **args)
+{
+	int i = 0;
+
+	while (args[i])
+		i++;
+	return (i);
 }
